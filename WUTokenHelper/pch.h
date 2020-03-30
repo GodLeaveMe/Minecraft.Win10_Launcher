21b83a73ded80d10a1ac884a4722bbc4da644b32
GodLeaveMe <45651732+GodLeaveMe@users.noreply.github.com>
2020-03-30 21:26:02 +0800

    Add files via upload

---
+++ WUTokenHelper/pch.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include <Windows.Foundation.h>
+#include <Windows.Foundation.Collections.h>
+#include <Windows.Security.Credentials.h>
+#include <Windows.Security.Authentication.Web.Core.h>
+#include <Windows.Security.Cryptography.h>
+#include "winrt/Windows.Internal.Security.Authentication.Web.h"
